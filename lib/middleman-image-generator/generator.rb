2f71eb601cecf1fdd8abd06104158690ed0ce4cd
noname00000123 <noname00000123@gmail.com>
2015-04-14 02:41:51 +1000

    Major refactor, trialing lovell/sharp node.js wrapper for libvips. Seems cleaner with ranging functionality.  Wil soon have ability to output dzi.

--- lib/middleman-image-generator/generator.rb
+++ lib/middleman-image-generator/generator.rb
@@ -25,6 +25,29 @@ module Middleman
       end
 
       def process_versions
+        batch = write_json(prepare_batch_paramaters)
+
+        # Generate enlargements when required
+        # options.sources.select{|item| item[:enlargements].present?}.each do |item|
+        #   manufacture_dzi(
+        #       input_path,
+        #       input_basename,
+        #       File.expand_path(item[:enlargements][:path])
+        #   )
+        # end
+
+        # Generate resized lossy compressed image files according to version parameters
+        manufacture(batch)
+
+        update_manifest
+
+        # TODO no longer necessary to add files to the sitemap now that they are put to source before_build
+        # manipulate_resource_list(new_resources)
+      end
+
+      def prepare_batch_paramaters
+        batch_parameters = []
+
         @content_types.each do |type|
           # Collect source files, if active generate required versions
           collect_specific(type).each do |file|
@@ -45,7 +68,7 @@ module Middleman
             # Thumbnails must then be small. We will set the image generator to produce versions to suit these conditions
             options.versions.each_with_index do |use_case, index|
               # Store the use case in a variable, it may be used in the output filename
-              $use_case = use_case.keys.to_s
+              $use_case = use_case.keys[0].to_s
 
               # A use case will have its own versions
               $versions = []
@@ -53,40 +76,17 @@ module Middleman
             end
 
             input  = {path: input_path, basename: input_basename}
-            output = versions($index, output_path, output_basename, $versions)
+            output = versions($index, output_path, output_basename, $versions).flatten
 
             batch = []
-            batch << [input: input, output: output[0]]
+            batch << [input: input, output: output]
 
-            # Generate resized lossy compressed image files according to version parameters
-            # generate_resized_versions(batch)
-
-            # Generate enlargements when required
-            # options.sources.select{|item| item[:enlargements].present?}.each do |item|
-            #   generate_dzi(
-            #       input_path,
-            #       input_basename,
-            #       File.expand_path(item[:enlargements][:path])
-            #   )
-            # end
+            batch_parameters << batch.flatten
           end
         end
 
-        update_manifest
-
-        # TODO no longer necessary to add files to the sitemap now that they are put to source before_build
-        # manipulate_resource_list(new_resources)
+        batch_parameters
       end
-
-      # Method taken from middleman-imageoptim by which images
-      # in sprockets load path were added to the sitemap.
-      # I am trying to push only the newly generated versions
-      # to the sitemap. Will this work?
-      # def manipulate_resource_list(resources)
-      #   return resources unless options.manifest
-      #
-      #   Middleman::ImageGenerator::ResourceList.manipulate(app, resources, options)
-      # end
 
       private
 
@@ -95,8 +95,8 @@ module Middleman
         # content_types = []
         # content_types << options.sources.each{|item| item[:type]}
 
-        # TODO ensure reversioned folder isn't collected
-        glob_rule = "#{@images_dir}/{#{@content_types.join(',')}}/**/*.{#{options.filetypes.join(',')}}"
+        # TODO ensure reversioned folder isn't collected filename must be of numeric
+        glob_rule = "#{@images_dir}/{#{@content_types.join(',')}}/**/[0-9]*.{#{options.filetypes.join(',')}}"
 
         # TODO refactor using Find
         # Find.find(@images_dir) do |path|
@@ -121,7 +121,9 @@ module Middleman
         # TODO ensure reversioned folder isn't collected
         # TODO account for different file structure in secondary_items
         # TODO refactor using Find would help with above
-        glob_rule = "#{@images_dir}/#{dir == 'secondary_items' ? 'secondary_items/{demonstrations,details,invites,studies}' : dir}/**/*.{#{options.filetypes.join(',')}}"
+        # TODO ensure user is aware of naming convention, filename must be numeric
+        # TODO new regex will not return contents of reversion dir
+        glob_rule = "#{@images_dir}/#{dir == 'secondary_items' ? 'secondary_items/{demonstrations,details,invites,studies}' : dir}/**/[0-9]*.{#{options.filetypes.join(',')}}"
 
         Dir[glob_rule]
       end
@@ -152,17 +154,6 @@ module Middleman
         end
       end
 
-      # Add filename in/output paths to parameters hash
-      #def versions(versions, input_path, input_basename, output_path, output_basename)
-      #   versions.each do |version, parameters|
-      #     parameters[:input]     = {basename: input_basename,
-      #                               path: input_path}
-      #
-      #     parameters[:output]    = {basename: "#{output_basename}_#{version}",
-      #                               path: File.join(output_path, "#{output_basename}_#{version}")}
-      #   end
-      # end
-
       # Revised. Arrange per version paramaters, filenames and dimensions
       def versions(index, output_path, output_basename, array)
         options.versions[index].map do |version|
@@ -173,71 +164,72 @@ module Middleman
           # Populate versions array with sizes
           sizes.each do |size|
             q = size[:quality]
-            w = size[:dims][:w]
-            h = size[:dims][:h]
+            min = size[:min_dimension]
 
             array << {
                 path: output_path,
-                basename: [output_basename, "q#{q}", "w#{w}"].join('_'),
+                basename: [output_basename, "q#{q}", "w#{min}"].join('_'),
                 use_case: $use_case,
                 quality: q,
-                width: w,
-                height: h
+                dim: min,
             }
 
-            # Multiply by a factor of two for inclusion of a double sized version for hi-dpi screens
+            # Multiply by a factor of two for inclusion
+            # of a double sized version for hi-dpi screens
             if options.retina_versions
-              w2x = w.to_i * 2
-              # ensure nil remains nil
-              h2x = h == nil ? nil : h.to_i * 2
+              double = min.to_i * 2
 
               array << {
                   path: output_path,
-                  basename: [output_basename, "q#{q}", "w#{w2x}_x2"].join('_'),
+                  basename: [output_basename, "q#{q}", "w#{double}_x2"].join('_'),
                   use_case: $use_case,
                   quality: q,
-                  width: w2x,
-                  height: h2x
+                  dim: double,
               }
             end
           end
 
-          # Return settings unique to versions required by this use case
-          settings
-          # And the versions array
-          array
+          # Return the settings and version parameters
+          # unique to this use case in an array
+          [array, settings]
         end
+      end
+
+      def write_json(collection)
+        tmp_dir      = File.join(Dir.pwd, 'source/assets/images/tmp')
+        tmp_filename = "#{tmp_dir}/batch#{Time.now.to_i}.json"
+
+        # Might do this in a seperate method, then loop through everything no need to call shell script per file. Do it once after we've caught everything in the file.
+        # Write json to tmp_file
+        File.open(tmp_filename, "w") do |file|
+          file.write(collection.to_json)
+        end
+
+        # return filename
+        tmp_filename
       end
 
       # For each of the required image types generate resized versions.
       # Add Copyright metadata and ICC profile http://www.color.org/srgbprofiles.xalter
-      def generate_resized_versions(output_versions)
-        IO.popen("node /home/user/Documents/webdev/staticgenerators/middleman/middleman-image-generator/lib/middleman-image-generator/resizer.js")
+      def manufacture(batch)
+        # TODO pay attention to the location of middleman-image-generator and also node.js installation as this arrangement may change.
+        # It seems, to run node, we have to return to user home
+        # We then have to point to resizer wherever it might be
+        path         = File.expand_path("..", Dir.pwd)
 
-#         output_versions.each do |version, parameters|
-#           input_path = "#{parameters[:input_path]}"
-#           output_path = "#{parameters[:output_path]}"
-#
-#           input_ext = File.extname(input_path)
-#           input_basename = File.basename(input_path, input_ext)
-#           input_dir = File.dirname(input_path)
-#
-#           # JPEG versions
-#           # WEBP versions, add xmp with google's webpmux see http://www.webmproject.org/code/
-#           IO.popen(
-#             %{vipsthumbnail #{input_path} --size #{parameters[:dimensions]}x1000 --interpolator nohalo --delete #{parameters[:options]} -o #{output_path}.jpg[#{parameters[:quality]},strip,optimize_coding]
-#
-# exiftool -tagsfromfile #{File.join(input_dir, input_basename)}.xmp -all:all -overwrite_original #{output_path}.jpg
-#
-# vipsthumbnail #{input_path} --size #{parameters[:dimensions]} --interpolator nohalo --delete #{parameters[:options]} -o #{output_path}.webp[#{parameters[:quality]},strip]
-#
-# webpmux -set xmp #{File.join(input_dir, input_basename)}.xmp #{output_path}.webp -o #{output_path}.webp}).read
-#
-#           new_resource(@app.sitemap, output_path, output_path)
-#         end
+        #cmd = "node #{path}/middleman-image-generator/lib/middleman-image-generator/resizer.js #{tmp_filename}"
+        cmd = "node /home/user/Documents/webdev/staticgenerators/middleman/middleman-image-generator/lib/middleman-image-generator/resizer.js #{batch}"
+        Dir.chdir(Dir.home){
+          #%x[cmd]
+          %x[`node /home/user/Documents/webdev/staticgenerators/middleman/middleman-image-generator/lib/middleman-image-generator/resizer.js /home/user/Documents/webdev/staticgenerators/middleman/olivialennon/source/assets/images/tmp/batch1428931948.json`]
+          #IO.popen(`node #{path}/middleman-image-generator/lib/middleman-image-generator/resizer.js`)
+          #IO.popen(cmd)
+        }
+
+        # TODO delete tmp_file after completion
       end
 
-      def generate_dzi(input_path, input_filename, output_dir)
+      def manufacture_dzi(input_path, input_filename, output_dir)
         ensure_enlargements_dir_exists(output_dir)
 
         # Append filename to output_dir
@@ -268,3 +260,46 @@ module Middleman
     end
   end
 end
+
+# Method taken from middleman-imageoptim by which images
+# in sprockets load path were added to the sitemap.
+# I am trying to push only the newly generated versions
+# to the sitemap. Will this work?
+# def manipulate_resource_list(resources)
+#   return resources unless options.manifest
+#
+#   Middleman::ImageGenerator::ResourceList.manipulate(app, resources, options)
+# end
+
+# Add filename in/output paths to parameters hash
+#def versions(versions, input_path, input_basename, output_path, output_basename)
+#   versions.each do |version, parameters|
+#     parameters[:input]     = {basename: input_basename,
+#                               path: input_path}
+#
+#     parameters[:output]    = {basename: "#{output_basename}_#{version}",
+#                               path: File.join(output_path, "#{output_basename}_#{version}")}
+#   end
+# end
+
+#         output_versions.each do |version, parameters|
+#           input_path = "#{parameters[:input_path]}"
+#           output_path = "#{parameters[:output_path]}"
+#
+#           input_ext = File.extname(input_path)
+#           input_basename = File.basename(input_path, input_ext)
+#           input_dir = File.dirname(input_path)
+#
+#           # JPEG versions
+#           # WEBP versions, add xmp with google's webpmux see http://www.webmproject.org/code/
+#           IO.popen(
+#             %{vipsthumbnail #{input_path} --size #{parameters[:dimensions]}x1000 --interpolator nohalo --delete #{parameters[:options]} -o #{output_path}.jpg[#{parameters[:quality]},strip,optimize_coding]
+#
+# exiftool -tagsfromfile #{File.join(input_dir, input_basename)}.xmp -all:all -overwrite_original #{output_path}.jpg
+#
+# vipsthumbnail #{input_path} --size #{parameters[:dimensions]} --interpolator nohalo --delete #{parameters[:options]} -o #{output_path}.webp[#{parameters[:quality]},strip]
+#
+# webpmux -set xmp #{File.join(input_dir, input_basename)}.xmp #{output_path}.webp -o #{output_path}.webp}).read
+#
+#           new_resource(@app.sitemap, output_path, output_path)
+#         end
